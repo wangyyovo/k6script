344d32692c2fac23195aa74b3ec9664c2de5d2cc
wangyyovo <626562203@qq.com>
2022-06-27 11:17:50 +0800

    update
    update

--- rpc.js
+++ rpc.js
@@ -2,38 +2,26 @@ import grpc from 'k6/net/grpc';
 import exec from 'k6/execution';
 import { Trend } from 'k6/metrics';
 import { check } from 'k6';
+import http from 'k6/http';
 
-
-
-let body = JSON.parse(open(__ENV.file));
+let body = readlocalFile(__ENV.body)
 let proto_path = __ENV.proto_path
 let proto_file = __ENV.proto_file
-let url = __ENV.url
-let address = __ENV.address
 
 const client = new grpc.Client();
 client.load(proto_path.split(','), proto_file);
 const grpcReqConnectingTrend = new Trend('grpc_req_connecting', true);
 
-
-
-
-
 export function setup() {
- return {body,url,address}
+  body = body || readRemoteFile(__ENV.body)
+  let url = __ENV.url
+  let address = __ENV.address
+
+  return {body,url,address}
 }
 
 export const options = {
-  discardResponseBodies: true,
-  // scenarios: {
-  //   contacts: {
-  //     executor: 'constant-vus',
-  //     vus: 1,
-  //     duration: '2s',
-  //     gracefulStop: '3s',
-  //   },
-
-  // },
+  // discardResponseBodies: true,
   summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(90)', 'p(95)', 'p(99)', 'p(99.9)', 'count'],
 };
 
@@ -41,53 +29,43 @@ export const options = {
 export default (data) => {
 
   if (__ITER == 0) {
+
     let startTime = new Date() - new Date(exec.scenario.startTime);
     client.connect(data.address, {
-      plaintext: true // 服务器没有配置证书需要将plaintext设为true,默认是false
+      plaintext: true // 鏈嶅姟鍣ㄦ病鏈夐厤缃瘉涔﹂渶瑕佸皢plaintext璁句负true,榛樿鏄痜alse
     });
     let endTime = new Date() - new Date(exec.scenario.startTime);
 
     grpcReqConnectingTrend.add(endTime - startTime);
   }
 
-  // request data
-  // const data = {
-  //   amount: 30,
-  //   area_id: 1,
-  //   buy_count: 3,
-  //   extend_info: {
-  //     Body: "2088102104143406",
-  //     Subject: "充值卡"
-  //   },
-  //   merchandise_code: "10095",
-  //   no_split: true,
-  //   order_type: 0,
-  //   pay_id: 1181,
-  //   pay_service_id: 1181,
-  //   server_id: 11,
-  //   sign: {
-  //     app: "app",
-  //     sign: "exercitation in non",
-  //     time: "sunt irure ullamco"
-  //   },
-  //   store_orderSerial: "111111111111",
-  //   user_ip: "192.168.249.34",
-  //   user_name: "qatest220407000010"
-  // };
-
   const response = client.invoke(data.url, data.body);
 
   check(response, {
     'status is OK': (r) => r && r.status === grpc.StatusOK,
   });
-
-  // console.log(JSON.stringify(response.message));
-  // client.close();
 };
 
 
-// export function teardown () {
-//   client.close();
-// }
+export function teardown () {
+  client.close();
+}
 
 
+function readlocalFile(fileName) {
+  let body;
+  if ((fileName.indexOf("http://")==-1) && (fileName.indexOf("https://")==-1)) {
+    body = JSON.parse(open(fileName));
+  }
+  return body
+}
+
+
+function readRemoteFile(fileName) {
+  let body;
+  if ((fileName.indexOf("http://")!=-1) || (fileName.indexOf("https://")!=-1)) {
+    const resp = http.get('https://jslib.k6.io/httpx/0.0.6/index.js');
+    body = resp.body;
+  }
+  return body
+}
