520255c7c6b4884995d36c715339ff2b4cb69c1d
wangyyovo <626562203@qq.com>
2022-06-27 15:03:25 +0800

    Update rpc.js

--- rpc.js
+++ rpc.js
@@ -4,23 +4,34 @@ import { Trend } from 'k6/metrics';
 import { check } from 'k6';
 import http from 'k6/http';
 
-let body = readlocalFile(__ENV.body)
-let proto_path = __ENV.proto_path
-let proto_file = __ENV.proto_file
+let body = readlocalFile(__ENV.body);
+let proto_path = __ENV.proto_path;
+let proto_file = __ENV.proto_file;
+
 
 const client = new grpc.Client();
 client.load(proto_path.split(','), proto_file);
 const grpcReqConnectingTrend = new Trend('grpc_req_connecting', true);
 
+
 export function setup() {
+
+
+  console.log(__ENV.vus);
+  console.log(__ENV.duration);
+  console.log(__ENV.iterations);
+
   body = body || readRemoteFile(__ENV.body)
-  let url = __ENV.url
-  let address = __ENV.address
+  let url = __ENV.url;
+  let address = __ENV.address;
 
   return {body,url,address}
 }
 
 export const options = {
+  vus: __ENV.vus,
+  duration: __ENV.duration,
+  iterations: __ENV.iterations,
   // discardResponseBodies: true,
   summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(90)', 'p(95)', 'p(99)', 'p(99.9)', 'count'],
 };
@@ -64,7 +75,7 @@ function readlocalFile(fileName) {
 function readRemoteFile(fileName) {
   let body;
   if ((fileName.indexOf("http://")!=-1) || (fileName.indexOf("https://")!=-1)) {
-    const resp = http.get('https://jslib.k6.io/httpx/0.0.6/index.js');
+    const resp = http.get(fileName);
     body = resp.body;
   }
   return body
