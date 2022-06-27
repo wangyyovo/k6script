4040dd58ed0905f6bf8f5b82d6e0868b6c39b4fa
wangyyovo <626562203@qq.com>
2022-06-27 19:21:40 +0800

    Update http.js

--- http.js
+++ http.js
@@ -2,10 +2,7 @@ import http from 'k6/http';
 import { check } from 'k6';
 import exec from 'k6/execution';
 
-let url = __ENV.api;
-let method = __ENV.method;
-let body = JSON.parse(open(__ENV.file));
-let type = __ENV.type
+let body = readlocalFile(__ENV.body)
 
 // export const options = {
 //   vus: 60, 			// 模拟10个虚拟用户(VU)
@@ -13,13 +10,17 @@ let type = __ENV.type
 //   //iterations: 10000,
 // };
 export const options = {
-
   // thresholds: {
   //   http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
   // },
 };
 
 export function setup() {
+  body = body || readRemoteFile(__ENV.body)
+  const type = __ENV.type
+  const method = __ENV.method;
+  const url = __ENV.url;
+
   if (url == undefined || url == '') {
     exec.test.abort('api参数没有设置');
   }
@@ -28,29 +29,29 @@ export function setup() {
     type = 'application/json';
   }
 
-  return {url,method,body,type}
+  const headers = { 'Content-Type': `${type}` };
+
+  return {url,method,body,type,headers}
 }
 
 export default function (data) {
 
-  const headers = { 'Content-Type': `${data.type}` };
-
   let res;
-  switch(method) {
+  switch(data.method) {
     case 'get':
-      res = http.get(data.url, JSON.stringify(data.body), { headers: headers });
+      res = http.get(data.url, JSON.stringify(data.body), { headers: data.headers });
       break;
     case 'post':
-      res = http.post(data.url, JSON.stringify(data.body), { headers: headers });
+      res = http.post(data.url, JSON.stringify(data.body), { headers: data.headers });
       break;
     case 'put':
-      res = http.put(data.url, JSON.stringify(data.body), { headers: headers });
+      res = http.put(data.url, JSON.stringify(data.body), { headers: data.headers });
       break;
     case 'patch':
-      res = http.patch(data.url, JSON.stringify(data.body), { headers: headers });
+      res = http.patch(data.url, JSON.stringify(data.body), { headers: data.headers });
       break;
     case 'del':
-      res = http.del(data.url, JSON.stringify(data.body), { headers: headers });
+      res = http.del(data.url, JSON.stringify(data.body), { headers: data.headers });
       break;
 
     default:
@@ -63,3 +64,20 @@ export default function (data) {
   });
 }
 
+
+function readlocalFile(fileName) {
+  let body;
+  if ((fileName.indexOf("http://")==-1) && (fileName.indexOf("https://")==-1)) {
+    body = JSON.parse(open(fileName));
+  }
+  return body
+}
+
+function readRemoteFile(fileName) {
+  let body;
+  if ((fileName.indexOf("http://")!=-1) || (fileName.indexOf("https://")!=-1)) {
+    const resp = http.get('https://jslib.k6.io/httpx/0.0.6/index.js');
+    body = resp.body;
+  }
+  return body
+}
