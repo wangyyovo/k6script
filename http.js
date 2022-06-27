fbc4b63cc4e4beb0f8a84640546346369fb2430d
wangyyovo <626562203@qq.com>
2022-06-27 19:41:26 +0800

    Update http.js

--- http.js
+++ http.js
@@ -4,34 +4,21 @@ import exec from 'k6/execution';
 
 let body = readlocalFile(__ENV.body)
 
-// export const options = {
-//   vus: 60, 			// 模拟10个虚拟用户(VU)
-//   duration: '2s', 	// 连续压测30秒
-//   //iterations: 10000,
-// };
 export const options = {
-  // thresholds: {
-  //   http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
-  // },
+
 };
 
 export function setup() {
   body = body || readRemoteFile(__ENV.body)
-  const type = __ENV.type
   const method = __ENV.method;
   const url = __ENV.url;
+ // const headers = JSON.parse(__ENV.header);
 
-  if (url == undefined || url == '') {
-    exec.test.abort('api参数没有设置');
-  }
+ const headers = {
+  "Content-Type":"application/json"
+ }
 
-  if (type == undefined || type == '') {
-    type = 'application/json';
-  }
-
-  const headers = { 'Content-Type': `${type}` };
-
-  return {url,method,body,type,headers}
+  return {url,method,body,headers}
 }
 
 export default function (data) {
@@ -76,7 +63,7 @@ function readlocalFile(fileName) {
 function readRemoteFile(fileName) {
   let body;
   if ((fileName.indexOf("http://")!=-1) || (fileName.indexOf("https://")!=-1)) {
-    const resp = http.get('https://jslib.k6.io/httpx/0.0.6/index.js');
+    const resp = http.get(fileName);
     body = resp.body;
   }
   return body
